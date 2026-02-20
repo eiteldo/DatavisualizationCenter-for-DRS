f87d9c109d819d3fe7e76bcbb011eb075e232489
eiteldo <141241920+eiteldo@users.noreply.github.com>
2026-02-20 09:04:32 +0100

    Update 1_Welcome.py

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -16,18 +16,6 @@ st.markdown( '''
             
     - Be patient while the calculations are running!
     - If you want to download images and text files, download the first and run the calculation again, then download the second. 
-    The results will be the same unless you change your input variables!
-    - Contact me using the Contact page in the sidebar and help to make the apps better!'''
+    The results will be the same unless you change your input variables!'''
             )
 
-st.markdown( '''### This is what your UV-Vis data plot and fit should look like:''')
-            
-image = Image.open('example_plot.png')
-
-st.image(image, caption='UV-Vis data fitted and plotted using this software', use_column_width='always')
-
-st.markdown( '''### This is what your Raman data plot should look like:''')
-            
-image = Image.open('example_plot_raman.png')
-
-st.image(image, caption='Raman data plotted using this software', use_column_width='always')
