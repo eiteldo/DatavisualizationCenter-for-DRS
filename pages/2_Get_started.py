cb98d2dbe621dc78259972ea02b2234d795af04e
eiteldo <141241920+eiteldo@users.noreply.github.com>
2026-02-20 10:29:46 +0100

    Update 2_Get_started.py

--- pages/2_Get_started.py
+++ pages/2_Get_started.py
@@ -4,11 +4,8 @@ from PIL import Image
 st.set_page_config(page_title="Get started", page_icon="🎓", layout='wide')
 
 st.markdown('''
-    **If you have problems running any of the applcations or you do not get the expected result, check this documentation.
-    For further help, use the contact form from the 'Contact' page.**
+    **If you have problems running any of the applcations or you do not get the expected result, check this documentation.**
     ''')
-
-# explanation on how to use the UV-Vis Tool
 
 st.header("How to use the UV-Vis tool")
 
@@ -17,45 +14,26 @@ st.markdown('''
     When you run the application, upload the file you want to use. The file should have the following layout:
     ''')
 
-# use expanders with images inside for keeping the page clean
-
 with st.expander("See image"):
     image = Image.open('file_drs.png')
     st.image(image, caption=None, use_column_width='always')
 
 st.markdown('''
     After uploading your file, you can see the imported dataframe with your data. You can use this to check if the imported data ist the corret format.
-    Type in the value for calculating the transition of your semiconductor:
+    Type in the value for calculating the transition of your sample, e.g.:
+    
     - for direct allowed transition type: 2.0
     - for indirect allowed transition type: 0.5
-         
-    **Forbidden transition types have not been tested!**
                       
-    Type the file name for your saved files. The necessary file extension will be added automatically!
+    Type the file name for your saved files. The necessary file extension will be added automatically! Fill in the number of datapoints the fitting algorithm has to use.
     ''')
 
-with st.expander("See image"):  
-    image = Image.open('df_drs.png')
-    st.image(image, caption=None, use_column_width='always')
-
-st.markdown('''Now you can see the actual graph of your modified data. Use the sliders below to select the fitting areas for the Tauc plot and for the y-offset plot.
-            As a result, you can see the selected areas in the next graph.''')
-
-with st.expander("See image"):
-    image = Image.open('select_slider_drs.png')
-    st.image(image, caption=None, use_column_width='always')
-
-
-st.markdown('''If you have selected you data areas, you can click the 'Start calculation' button.
+st.markdown('''Click the 'Start calculation' button.
 This will start your calculations and will give you the final plot and values as an output.
-You can then download the graph using the 'Download image' button.
-If you need the values, just re-run the calculation and download the text file.''')
+You can then download the graph and the values as as .txt using the 'Download' buttons.''')
 
-with st.expander("See image"):
-    image = Image.open('results_drs.png')
-    st.image(image, caption=None, use_column_width='always')
 
-# Explanation on how to use the Raman plotter
+# Explanation on how to use the TEM analysis tool
 
 st.header("How to use the TEM analysis tool")
 
@@ -72,6 +50,7 @@ st.markdown('''
     After uploading your file, you can directly see the analyzed data from your image.
     For downloading the image use the 'Download image' button.
     ''')
+
 
 
 
